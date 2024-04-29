7d92ed9884c98bff453d5f6a5f02241be730076b
islexu <47247886+islexu@users.noreply.github.com>
2024-04-30 00:34:48 +0200

    init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,6 @@
 # and any modifications thereto. Any use, reproduction, disclosure or
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
-#
 
 from omni.isaac.examples.user_examples.deformableObjectGrasping import deformableObjectGrasping
 from omni.isaac.examples.user_examples.deformableObjectGrasping_extension import deformableObjectGrasping_extension
